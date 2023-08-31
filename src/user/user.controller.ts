2192f591c191e74838fc001b3e0bdba3609d305c
Arthur Gomes <arthur.gomes@familiapires.com.br>
2023-08-31 20:36:04 -0300

    add controller createUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import {
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { UserService } from './user.service';
+import { UserDto } from './dto/response/user.dto';
+import { CreateUserDto } from './dto/request/create-user.dto';
 
-@Controller('user')
-export class UserController {}
+@ApiTags('User')
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  @ApiOperation({
+    summary: 'Cria um novo usuário',
+  })
+  @ApiCreatedResponse({ type: UserDto })
+  @ApiConflictResponse({
+    description: 'Usuário já cadastrado',
+  })
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    return await this.userService.createUser(createUserDto);
+  }
+}
