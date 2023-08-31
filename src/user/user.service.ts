55cf0591fb55b406fe31cc0e3381d9bd69dddf6e
Arthur Gomes <arthur.gomes@familiapires.com.br>
2023-08-31 20:35:53 -0300

    add service createUser

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/request/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  public async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const checkUser = await this.userRepository.findOne({
+      where: [{ email: createUserDto.email }],
+    });
+
+    if (checkUser) {
+      throw new ConflictException('user already exists');
+    }
+
+    return await this.userRepository.create(createUserDto).save();
+  }
+}
