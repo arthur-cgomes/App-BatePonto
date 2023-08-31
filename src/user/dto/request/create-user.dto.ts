748f56007f3db3b43d70e2ad8fc3a733e7a95004
Arthur Gomes <arthur.gomes@familiapires.com.br>
2023-08-31 20:35:23 -0300

    add dto user

---
+++ src/user/dto/request/create-user.dto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({ type: String })
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty()
+  @IsOptional()
+  birthDate: Date;
+
+  @ApiProperty()
+  @IsOptional()
+  phone: string;
+}
