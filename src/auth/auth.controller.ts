19b96679b76f451e0abdecc2f6a08133d4e3dc85
Arthur Gomes <arthur.gomes@familiapires.com.br>
2023-09-02 11:30:13 -0300

    settings user authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { AuthPayload } from './interfaces/auth.interface';
 
+@ApiTags('Auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @ApiOperation({
+    summary: 'Autentica o usuário',
+    description: `Roles: ${process.env.ALL}`,
+  })
+  @ApiResponse({
+    status: HttpStatus.FORBIDDEN,
+    description: 'Senha inválida',
+  })
+  @Post()
+  async login(@Body() auth: AuthPayload) {
+    return await this.authService.validateUserByPassword(auth);
+  }
+}
