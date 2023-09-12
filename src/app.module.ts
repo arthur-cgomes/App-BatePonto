4f03d7a7f5184794278a290947909d05b4bba015
Arthur Gomes <arthur.gomes@familiapires.com.br>
2023-09-11 21:26:15 -0300

    remove userType

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppDataSource } from './datasource';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './modules/user/user.module';
-import { UserTypeModule } from './modules/user-type/user-type.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
@@ -10,7 +9,6 @@ import { AuthModule } from './modules/auth/auth.module';
     TypeOrmModule.forRoot(AppDataSource.options),
     UserModule,
     AuthModule,
-    UserTypeModule,
   ],
   controllers: [],
   providers: [],
--- src/modules/user-type/user-type.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserTypeService } from './user-type.service';
-import { UserTypeController } from './user-type.controller';
-
-@Module({
-  providers: [UserTypeService],
-  controllers: [UserTypeController],
-})
-export class UserTypeModule {}
