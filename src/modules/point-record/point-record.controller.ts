29781d7003946cc4c683bb1e8e6723be3ff3066a
Arthur Gomes <arthur.gomes@familiapires.com.br>
2023-09-14 20:04:27 -0300

    settings service

--- src/modules/point-record/point-record.controller.ts
+++ src/modules/point-record/point-record.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiBearerAuth, ApiConflictResponse, ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { PointRecordService } from './point-record.service';
+import { CreatePointRecordDto } from './dto/request/create-point-record.dto';
+import { PointRecordDto } from './dto/response/point-record.dto';
 
-@Controller('point-record')
-export class PointRecordController {}
+@ApiBearerAuth()
+@ApiTags('Point-Record')
+@Controller('point-records')
+export class PointRecordController {
+  constructor(private readonly pointRecordService: PointRecordService) {}
+
+  @Post()
+  @ApiOperation({
+    summary: 'Cria um novo registro de ponto',
+  })
+  @ApiCreatedResponse({ type: PointRecordDto })
+  @ApiConflictResponse({
+    description: 'Já existe um registro desse tipo para este usuário hoje'
+  })
+  async createPointRecord(@Body() createPointRecord: CreatePointRecordDto) {
+    return await this.pointRecordService.createPointRecord(createPointRecord);
+  }
+}
--- src/modules/point-record/point-record.service.ts
+++ src/modules/point-record/point-record.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PointRecord } from './entity/point-record.entity';
+import { CreatePointRecordDto } from './dto/request/create-point-record.dto';
+import { Repository } from 'typeorm';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class PointRecordService {}
+export class PointRecordService {
+  constructor(
+    private readonly userService: UserService,
+    @InjectRepository(PointRecord)
+    private readonly pointRecordRepository: Repository<PointRecord>,
+  ) {}
+
+  async createPointRecord(createPointRecordDto: CreatePointRecordDto): Promise<PointRecord> {
+    const { userId, pointRecordType } = createPointRecordDto;
+    const currentDate = new Date();
+    currentDate.setHours(0, 0, 0, 0); 
+
+    await this.userService.getUserById(userId);
+
+    const existingRecord = await this.pointRecordRepository.findOne({
+      where: {
+        userId,
+        pointRecordType,
+        dateTime: currentDate,
+      },
+    });
+
+    if (existingRecord) {
+      throw new ConflictException('such a record already exists for this user today');
+    }
+
+    const newPointRecord = this.pointRecordRepository.create(createPointRecordDto);
+    return await this.pointRecordRepository.save(newPointRecord);
+  }
+}
